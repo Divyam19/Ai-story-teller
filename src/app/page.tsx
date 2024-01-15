34cecce0a80f27b078c4b8d4a1d8dfe53b2c0bd6
Divyam19 <divyamgupta19@gmail.com>
2024-01-15 20:12:54 +0530

    added login form using cards and button from shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 import dexter from '../assets/d4aad59fad96ff5b2900206f9e8dcc12.jpg'
+import Nav from '@/components/Nav'
+import { Card, CardContent, CardHeader } from '@/components/ui/card'
 export default function Home() {
   return (
+    <>
     <div className='flex h-screen'>
       {/*60 side*/}
       <div className='w-3/5 bg-yellow-400'>
@@ -10,7 +13,31 @@ export default function Home() {
         className='h-screen w-full'/>
       </div>
       {/* 40 side */}
-      <div className='w-2/5 bg-red-500'></div> 
-    </div> 
+      <div className='w-2/5 bg-violet-600 flex items-center justify-center'>
+        
+        <Card className='m-5'>
+          <CardHeader>Login/Signup</CardHeader>
+          <CardContent>
+            <form className=''>
+              <label>Email</label>
+              <br/>
+              <input type='email' className='bg-gray-400'/>
+              <br/>
+              <br/>
+              <label>Password</label>
+              <br/>
+              <input type='password' className='bg-gray-400'/>
+              <br/>
+              <div className='flex gap-4 pt-3'>
+                <Button type='submit'>Submit</Button>
+                <Button type='reset'>Reset</Button>
+              </div>
+            </form>
+          </CardContent>
+        </Card>
+        
+      </div> 
+    </div>
+    </> 
   )
 }
---
+++ src/components/Nav.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const  Nav = () => {
+  return (
+    <div className='flex items-center justify-between m-5'> 
+        <h3>NAVBAR</h3>
+    </div>
+  )
+}
+
+export default Nav; 
